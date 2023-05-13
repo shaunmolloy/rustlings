1aa369f3fd9eb23678ba13554336f072fe8654b0
Shaun Molloy <shaun.molloy@rio.ai>
2023-05-13 12:46:30 +0100

    complete quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let mut price = 2;
 
-// Put your function here!
-// fn calculate_price_of_apples {
+    if quantity > 40 {
+        price = 1;
+    }
+
+    quantity * price
+}
 
 // Don't modify this function!
 #[test]
