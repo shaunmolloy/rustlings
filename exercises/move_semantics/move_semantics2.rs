ee04124cf5ad9f0adc54b30f0c516bdd49bf2dcc
Shaun Molloy <shaun.molloy@rio.ai>
2023-05-15 22:54:07 +0100

    complete move semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,13 +5,11 @@
 // vec0 has length 3 content `[22, 44, 66]`
 // vec1 has length 4 content `[22, 44, 66, 88]`
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = fill_vec([22, 44, 66]);
 
     // Do not move the following line!
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec([22, 44, 66]);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -21,12 +19,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(data: [i32; 3]) -> Vec<i32> {
+    let mut vec = Vec::new();
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    vec.push(data[0]);
+    vec.push(data[1]);
+    vec.push(data[2]);
 
     vec
 }
